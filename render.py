70d07f6cb605f2beaa1a853bbc8cd3e7f1c05e0c
codeapps <codeapps@aol.com>
2016-10-15 16:34:58 -0500

    Create Render Boilerplate for PUG

---
+++ render.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+file = open(category + ".pug", "w+")
+    file.write(name);
+file.close()
+
+
+# CLI arg[1] --render <category_id>
+    # $categoryId = <category_id>
+    # def searchCategoryId(id)
+        # L1 = 35
+        # if (id > AggregateNode)
+
+        # SEARCH <category_id> IN CategoryLevel
+        # GENERATE <category_id>.html
+        # CATEGORY TREE ROOTED
