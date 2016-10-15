1e8a88a0a13b0ceb4dc035e041c6e9fd0c85349b
codeapps <codeapps@aol.com>
2016-10-15 16:33:27 -0500

    Return Value inside .ENV

--- env.py
+++ env.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 def env(name):
-    envFile = open(".env", "w+")
-        for line in envFile:
+    file = open(".env", "w+")
+        for line in file:
            line = line.split("=")
-           print line[0]
+           try:
+               if line[0] == name:
+                   return line[1]
+           except Exception as e:
+               raise
     file.close()
