1b68c52a073d6a5bfea6f4db2fed2d7431ea1550
codeapps <codeapps@aol.com>
2016-10-15 17:59:06 -0500

    Refactor Query Function

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ class DB(object):
     """docstring for DB."""
 
     def __init__(self, name):
-        name = name + ".db"
+        name = name + ".sqlite"
         self.name = name
         self.conn = None
         self.cur = None
@@ -16,31 +16,42 @@ class DB(object):
 
     def create(self):
         if os.path.isfile(self.name):
+            print("File %s Exist" % self.name)
             pass
         else:
             try:
                 file = open(self.name, "w+")
+                print("Creating %s" % self.name)
             except Exception as e:
-                print(e)
                 raise
 
     def connect(self):
         try:
             self.conn = sqlite3.connect(self.name)
-            self.cur = self.conn.cursor();
+            self.cur = self.conn.cursor()
+            print("Connecting to %s" % self.name)
+            return self.cur
         except Exception as e:
-            print(e)
+            raise
+
+    def close(self):
+        try:
+            if self.conn != None:
+                print("Closing Conection Manually")
+                return self.conn.close()
+            else:
+                print("Please Connect the Database")
+        except Exception as e:
             raise
 
     def __del__(self):
         try:
-            if self.conn != None and self.cur != None:
-                print("Closing Conection by Object Deletion or Program Exit")
-                return self.cur.close()
+            if self.conn != None:
+                print("Secure Conection Close by Object Deletion or Program End")
+                return self.conn.close()
             else:
-                print("Please Connect the Database")
+                print("The Database is Closed Now")
         except Exception as e:
-            print(e)
             raise
 
     def drop(self):
@@ -49,30 +60,54 @@ class DB(object):
                 os.remove(self.name)
                 print("Sucessful Delete Filename %s" % self.name)
         except Exception as e:
-            print(e)
             raise
 
     def commit(self):
         try:
-            return self.conn.commit();
+            return self.cur.commit();
         except Exception as e:
             raise
 
-    def query(self, name, query, values):
-        if type(name) is str and type(query) is str and values is str:
-            if query == 'create':
-                query = "CREATE TABLE %s;" % name
-            elif query == 'drop':
-                query = "DROP TABLE %s;" % name
-            elif query == 'insert':
-                query = "INSERT INTO %s VALUES %s" % self.name, values
+    def createTable(self, tableName, tableFields):
+        if type(tableName) is str and type(tableFields) is dict:
+            query = "CREATE TABLE %s %s" % (tableName, for x in tableFields : x + ' ' + x[value])
             try:
-                return self.conn.execute(query)
+                print("Create Table Sucessful")
+                return self.cur.execute(query)
             except Exception as e:
-                print(e)
                 raise
+
+    def dropTable(self, tableName):
+        if type(tableName) is str:
+            query = "DROP TABLE %s" % tableName
+            try:
+                print("Drop Table Sucessful")
+                return self.cur.execute(query)
+            except Exception as e:
+                raise
+
+    def insertTable(self, tableName, tableFieldsValues):
+        if type(tableName) is str and type(tableFieldsValues) is dict:
+            query = "INSERT INTO %s VALUES %s" % (tableName, tableFieldsValues)
+            try:
+                print("Insert Values Sucessful")
+                return self.cur.execute(query)
+            except Exception as e:
+                raise
+        else:
+            raise Exception("Query Must Be String")
 
 ebay = DB('ebay')
 ebay.connect()
-ebay.query('', '')
+
+fields = {
+    'BestOfferEnabled' : 'boolean',
+    'AutoPayEnable' : 'boolean',
+    'CategoryID' : 'integer',
+    'CategoryName' : 'string',
+    'CategoryParentID' : 'integer'
+}
+
+ebay.createTable('daniel', fields)
+ebay.close()
 ebay.drop()
