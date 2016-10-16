af7a8ef2e08f940afcc1eeaba6751305fad65f14
codeapps <codeapps@aol.com>
2016-10-16 15:27:27 -0500

    Fetch Complete

--- curl.py
+++ curl.py
@@ -2,10 +2,11 @@
 
 import urllib.request
 import urllib.parse
+import urllib.error
 import json
 from env import env
 
-class curl(object):
+class CURL(object):
 
     """docstring for CURL."""
 
@@ -15,39 +16,15 @@ class curl(object):
         self.response = None
 
     def requestXML(self, headers, data):
-        try:
-            if isinstance(headers, dict) and isinstance(data, dict):
-                data = urllib.parse.urlencode(data).encode("utf-8")
-                data = data.encode('ascii')
+        if isinstance(headers, dict) and isinstance(data, str):
+            data = data.replace("\n", "")
+            data = data.encode('ascii')
+            try:
+                print("Fetching...")
                 self.request = urllib.request.Request(self.url, headers=headers, data=data)
-                self.response = urllib.request.urlopen(self.request)
-                return self.request
-            else:
-                print("XML has been String Type and Headers a Dictionary")
-        except Exception as e:
-            raise
-
-headers = {
-    'X-EBAY-API-CALL-NAME' : 'GetCategories',
-    'X-EBAY-API-APP-NAME' : env('APP_NAME'),
-    'X-EBAY-API-CERT-NAME' : env('CERT_NAME'),
-    'X-EBAY-API-DEV-NAME' : env('EBAY_API_DEV_NAME'),
-    'X-EBAY-API-SITEID' : '0',
-    'X-EBAY-API-COMPATIBILITY-LEVEL' : '861',
-    'Content-Type' : 'text/xml'
-}
-
-data = {
-   "GetCategoriesRequest": {
-      "RequesterCredentials": {
-         "eBayAuthToken": env('EBAY_AUTH_TOKEN')
-      },
-      "CategorySiteID": "0",
-      "DetailLevel": "ReturnAll",
-      "_xmlns": "urn:ebay:apis:eBLBaseComponents"
-   }
-}
-
-ebayAPI = curl('https://api.sandbox.ebay.com/ws/api.dll')
-ebayAPI.requestXML(headers, data)
-print(ebayAPI.request)
+                self.response = urllib.request.urlopen(self.request).read()
+                return self.response
+            except urllib.error.URLError as e:
+                print(e.reason)
+        else:
+            print("Headers needs be Type Dictionary and Data Type String")
