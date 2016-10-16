fcbf9e8396efe208d4efb4bf1507a80b09f2ee73
codeapps <codeapps@aol.com>
2016-10-16 11:09:12 -0500

    Return Parse

--- env.py
+++ env.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+import os
+
 def env(name):
-    file = open(".env", "w+")
-        for line in file:
-           line = line.split("=")
-           try:
-               if line[0] == name:
-                   return line[1]
-           except Exception as e:
-               raise
-    file.close()
+    if os.path.isfile(".env"):
+        env = open(".env", "r")
+        data = env.readlines()
+        for line in data:
+            line = line.split("=")
+            try:
+                if line[0] == name:
+                    return line[1].replace("\n", "")
+            except Exception as e:
+                raise
+        file.close()
+    else:
+        print("ENV File Not Found")
