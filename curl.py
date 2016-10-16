97cdb871c3bb6d368e247f4de21f12469f9331e3
codeapps <codeapps@aol.com>
2016-10-16 14:04:43 -0500

    Parsing Headers, Data and Encode ASCII Bytes

--- curl.py
+++ curl.py
@@ -1,60 +1,53 @@
 #!/usr/bin/env python
 
-import urlib2
+import urllib.request
+import urllib.parse
+import json
+from env import env
 
 class curl(object):
 
     """docstring for CURL."""
 
     def __init__(self, url):
-        super(CURL, self).__init__()
         self.url = url
-        self.req = None
-        self.cur = None
-        self.request()
+        self.request = None
+        self.response = None
 
-    def request(self):
+    def requestXML(self, headers, data):
         try:
-            self.req = urllib2.Request(self.url)
-            self.cur = self.req.cursor()
-            return self.cur
-        except Exception as e:
-            raise
-
-    def requestXML(self, headers, xml):
-        try:
-            if isinstance(xml, str) and isinstance(headers, dict):
-                for header, value in headres.items():
-                    self.cur.add_header(header[value])
-                    form = {'XML': xml}
-                    data = urllib.urlencode(form)
-                    self.cur.add_data(xml)
-                res = urllib2.urlopen(req)
-                return res.read()
+            if isinstance(headers, dict) and isinstance(data, dict):
+                data = urllib.parse.urlencode(data).encode("utf-8")
+                data = data.encode('ascii')
+                self.request = urllib.request.Request(self.url, headers=headers, data=data)
+                self.response = urllib.request.urlopen(self.request)
+                return self.request
             else:
                 print("XML has been String Type and Headers a Dictionary")
         except Exception as e:
             raise
 
 headers = {
-    'X-EBAY-API-CALL-NAME:' : 'GeteBayOfficialTime',
-    'X-EBAY-API-APP-NAME:' : env('APP_NAME'),
-    'X-EBAY-API-CERT-NAME:' : env('CERT_NAME'),
-    'X-EBAY-API-DEV-NAME:' : env('EBAY_API_DEV_NAME'),
-    'X-EBAY-API-SITEID:' : '0',
-    'X-EBAY-API-COMPATIBILITY-LEVEL:' : '861'
+    'X-EBAY-API-CALL-NAME' : 'GetCategories',
+    'X-EBAY-API-APP-NAME' : env('APP_NAME'),
+    'X-EBAY-API-CERT-NAME' : env('CERT_NAME'),
+    'X-EBAY-API-DEV-NAME' : env('EBAY_API_DEV_NAME'),
+    'X-EBAY-API-SITEID' : '0',
+    'X-EBAY-API-COMPATIBILITY-LEVEL' : '861',
+    'Content-Type' : 'text/xml'
 }
 
-xml = """
-<?xml version="1.0" encoding="utf-8"?>
-<GetCategoriesRequest xmlns="urn:ebay:apis:eBLBaseComponents">
-  <RequesterCredentials>
-  <eBayAuthToken>""" + env('EBAY_AUTH_TOKEN') + """</eBayAuthToken>
-  </RequesterCredentials>
-  <CategorySiteID>0</CategorySiteID>
-  <DetailLevel>ReturnAll</DetailLevel>
-</GetCategoriesRequest>
-"""
+data = {
+   "GetCategoriesRequest": {
+      "RequesterCredentials": {
+         "eBayAuthToken": env('EBAY_AUTH_TOKEN')
+      },
+      "CategorySiteID": "0",
+      "DetailLevel": "ReturnAll",
+      "_xmlns": "urn:ebay:apis:eBLBaseComponents"
+   }
+}
 
 ebayAPI = curl('https://api.sandbox.ebay.com/ws/api.dll')
-ebayAPI.requestXML(headers, xml)
+ebayAPI.requestXML(headers, data)
+print(ebayAPI.request)
