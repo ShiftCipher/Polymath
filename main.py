2774b0dc383b63391fb30d756af93a841b41ce90
codeapps <codeapps@aol.com>
2016-10-17 17:43:45 -0500

    Assambly

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python
 
-__author__      = "Daniel Tarazona"
-__copyright__   = "Copyright 2016, Codeapps"
+__author__     = "Daniel Tarazona"
+__copyright__  = "Copyright 2016, Codeapps"
 
 import curl
-import env
 import db
-import XML
+import curl
+import request
 import sys
+from env import env
 
 def main():
-    if sys.argv[0] == "generate":
-        ebayDB = DB('ebay')
-        ebayHeaders = {
-            'X-EBAY-API-CALL-NAME' : 'GetCategories',
-            'X-EBAY-API-APP-NAME' : env('APP_NAME'),
-            'X-EBAY-API-CERT-NAME' : env('CERT_NAME'),
-            'X-EBAY-API-DEV-NAME' : env('EBAY_API_DEV_NAME'),
-            'X-EBAY-API-SITEID' : '0',
-            'X-EBAY-API-COMPATIBILITY-LEVEL' : '861',
-            'Content-Type' : 'text/xml'
-        }
-        ebayXML = XML('ebayGetCategories')
-        ebayXML.setRequesterCredentials("EBAY_AUTH_TOKEN")
-        ebayAPI = CURL(env('EBAY_API_ENDPOINT'))
-        ebayCategoryArray = ebayAPI.requestXML(ebayHeaders, ebayXML)
-        ebayDB.close()
-    elif len(sys.argv) > 1:
+    ebayAPI = curl.API('EBAY_API_URL')
+    #header = request.Head()
+    #header.addHeader('X-EBAY-API-CALL-NAME', 'GetCategories')
+    #header.addHeader('X-EBAY-API-APP-NAME', env('APP_NAME'))
+    #header.addHeader('X-EBAY-API-CERT-NAME', env('CERT_NAME'))
+    #header.addHeader('X-EBAY-API-DEV-NAME', env('EBAY_API_DEV_NAME'))
+    #header.addHeader('X-EBAY-API-SITEID', '0')
+    #header.addHeader('X-EBAY-API-COMPATIBILITY-LEVEL', '989')
+    #header.addHeader('Content-Type', 'text/xml')
+    #ebayAPIGetCategoriesHeaders = header.getAll()
+    #ebayAPIGetCategoriesData = request.Body('ebayGetCategories', 'EBAY_AUTH_TOKEN').getXML()
+    #ebayAPICategories = ebayAPI.requestXML(ebayAPIGetCategoriesHeaders, ebayAPIGetCategoriesData)
+    ebayDB = db.DB('ebay')
+    ebayDB.connect()
+
+    columns = ebayAPI.parseXMLColumns('Category', 'xml/ebayCategories.xml')
+    ebayDB.createTablesByColumn('CategoryLevel', columns, 6)
+    columnsName = " ".join(columns.keys())
+    ebayDB.parseXML('Category', columnsName, 'xml/ebayCategories.xml')
+
+    ebayDB.commit()
+
+    # ebayAPI.exportXML('ebayCategories')
+    ebayDB.close()
+    if len(sys.argv) > 1:
         if sys.argv[1] == "--rebuild":
             ebayDB.drop()
             ebayDB.create()
