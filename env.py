47264e37398376f69c9c060f47a0473e5984fef2
codeapps <codeapps@aol.com>
2016-10-17 17:44:14 -0500

    Ready to Replace with Configparser

--- env.py
+++ env.py
@@ -2,27 +2,17 @@
 
 import os
 
-class env(object):
-
-    """docstring for ENV."""
-
-    def __init__(self, name):
-        self.arg = name
-
-    def env(self):
-        if os.path.isfile(".env"):
-            env = open(".env", "r")
-            data = env.readlines()
-            for line in data:
-                line = line.split("=")
-                try:
-                    if line[0] == self.name:
-                        return str(line[1].replace("\n", ""))
-                except Exception as e:
-                    raise
-            file.close()
-        else:
-            print("ENV File Not Found")
-
-    def __call__(self):
-        return self.env()
+def env(name):
+    if os.path.isfile(".env"):
+        env = open(".env", "r")
+        data = env.readlines()
+        for line in data:
+            line = line.split("=")
+            try:
+                if line[0] == name:
+                    return str(line[1].replace("\n", ""))
+            except Exception as e:
+                raise
+        env.close()
+    else:
+        print("ENV File Not Found")
